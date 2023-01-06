5d53b39694e2a37ad3dd50c62d1e86821da6d50a
Developer <114725350+SusanStorm@users.noreply.github.com>
2023-01-06 18:08:17 +0530

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,7 @@ export const signup = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 12)
         const newUser = await users.create({ name, email, password: hashedPassword }) 
         const token = jwt.sign({ email: newUser.email, id:newUser._id}, process.env.JWT_SECRET , { expiresIn: '1h'});
-        res.status(200).json({ result: newUser, token })
+        res.status(200).json({ result: newUser, token,sucess:true })
     } catch(error){
         res.status(500).json("Something went worng...")
     }
@@ -33,7 +33,7 @@ export const login = async (req, res) => {
             return res.status(200).json({message : "Invalid credentials",sucess:false})
         }
         const token = jwt.sign({ email: existinguser.email, id:existinguser._id},  process.env.JWT_SECRET, { expiresIn: '1h'});
-        res.status(200).json({ result: existinguser, token,})
+        res.status(200).json({ result: existinguser, token,sucess:true})
     } catch (error)  {
         res.status(500).json({message:"Something went worng...",sucess: false})
     }
